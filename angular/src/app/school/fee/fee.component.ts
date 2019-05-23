8a3c08a5b8533d9e1b650fe0f6c1f39e13387445
karthikmuthyam1 <karthikmuthyam1@gmail.com>
2019-05-23 19:09:53 +0530

    added fee moodule

---
+++ angular/src/app/school/fee/fee.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-departments',
+    templateUrl: './departments.component.html',
+    styleUrls: ['./departments.component.css']
+  })
+  export class DepartmentsComponent implements OnInit {
+   
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+  }
---
+++ angular/src/app/school/fee/fee.ts
@@ -0,0 +1,12 @@
+export class Fee
+{
+    feeType: string;
+    isTerm:boolean;
+    isHostel:boolean;
+    termsCount:number;
+    feeAmount:string;
+    paidMode:string;
+    className:string;
+    section:string;
+    student:string;
+}
